ba6864cc8a78548fd3dc956ee191ac19fb3febf9
AlcoRV <zennerqwert@ya.ru>
2023-06-13 12:49:49 +0300

    added null and IsRight checking

--- CustomGeometry.Tests/Geometry2DExtensionTests.cs
+++ CustomGeometry.Tests/Geometry2DExtensionTests.cs
@@ -3,13 +3,13 @@
 namespace CustomGeometry.Tests
 {
     [TestClass()]
-    public class Geometry2DExtensionTests
+    public sealed class Geometry2DExtensionTests
     {
         [TestMethod]
         [DataRow(1, Math.PI)]
         [DataRow(10, Math.PI * 100)]
         [DataRow(7.34, Math.PI * 7.34 * 7.34)]
-        public void CalculateAreaTestCircle_WithPositiveRadius_ReturnsCorrectResult(double radius, double expected)
+        public void CalculateAreaExtCircle_WithPositiveRadius_ReturnsCorrectResult(double radius, double expected)
         {
             // Arrange
             var circle = new Circle(radius);
@@ -26,7 +26,7 @@ namespace CustomGeometry.Tests
         [DataRow(0, 1)]
         [DataRow(-1, Math.PI)]
         [ExpectedException(typeof(ArgumentException), "Radius is not valid!")]
-        public void CalculateAreaTestCircle_WithZeroOrNegativeRadius_ReturnsException(double radius, double expected)
+        public void CalculateAreaExtCircle_WithZeroOrNegativeRadius_ReturnsException(double radius, double expected)
         {
             // Arrange
             var circle = new Circle(radius);
@@ -39,10 +39,23 @@ namespace CustomGeometry.Tests
         }
 
         [TestMethod]
+        public void CalculateAreaExtCircle_WithNullRef_ReturnsCorrectResult()
+        {
+            // Arrange
+            Circle circle = null;
+
+            // Act
+            double actual = circle.CalculateAreaExt();
+
+            // Assert
+            Assert.AreEqual(0, actual, 0.0001);
+        }
+
+        [TestMethod]
         [DataRow(1, 1, 1, 0.433013)]
         [DataRow(2, 3, 4, 2.904738)]
         [DataRow(1, 2, 3, 0)]
-        public void CalculateAreaTestTriangle_WithPositiveLegs_ReturnsCorrectResult(double a, double b, double c, double expected)
+        public void CalculateAreaExtTriangle_WithPositiveLegs_ReturnsCorrectResult(double a, double b, double c, double expected)
         {
             // Arrange
             var triangle = new Triangle(a, b, c);
@@ -62,7 +75,7 @@ namespace CustomGeometry.Tests
         [DataRow(1, -1, 1, 0)]
         [DataRow(1, 1, -1, 0)]
         [ExpectedException(typeof(ArgumentException), "One of arguments is not valid!")]
-        public void CalculateAreaTestTriangle_WithNullOrNegativeLegs_ReturnsException(double a, double b, double c, double expected)
+        public void CalculateAreaExtTriangle_WithZeroOrNegativeLegs_ReturnsException(double a, double b, double c, double expected)
         {
             // Arrange
             var triangle = new Triangle(a, b, c);
@@ -72,6 +85,19 @@ namespace CustomGeometry.Tests
 
             // Assert
             Assert.AreEqual(expected, actual, 0.0001);
+        }
+
+        [TestMethod]
+        public void CalculateAreaExtTriangle_WithNullRef_ReturnsCorrectResult()
+        {
+            // Arrange
+            Triangle triangle = null;
+
+            // Act
+            double actual = triangle.CalculateAreaExt();
+
+            // Assert
+            Assert.AreEqual(0, actual, 0.0001);
         }
 
         [TestMethod()]
@@ -89,6 +115,19 @@ namespace CustomGeometry.Tests
 
             // Assert
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsRight_WithNullRef_ReturnsCorrectResult()
+        {
+            // Arrange
+            Triangle triangle = null;
+
+            // Act
+            bool actual = triangle.IsRightExt();
+
+            // Assert
+            Assert.AreEqual(false, actual);
         }
     }
 }
